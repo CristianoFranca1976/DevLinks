43f0bfa68d02e31139dc4829deea96131b8f8470
CrstianoFranca1976 <cristiano.franca@hotmail.co.uk>
2023-06-13 19:41:38 +0100

    mudanca de pasta

---
+++ .vscode/script.js
@@ -0,0 +1,18 @@
+function togglemode() {
+const html = document.documentElement
+html.classList.toggle("light")
+
+const img = document.querySelector("#profile img")
+    
+if (html.classList.contains("light")) {
+
+    img.setAttribute("src", "assets/avatar-depois.png")
+    
+} else {
+    
+img.setAttribute("src", "./assets/avatar-antes.png")
+    
+}
+
+
+}
